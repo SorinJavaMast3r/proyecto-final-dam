431ace9547d6962b1ac441285b109e421d5ab9d5
SorinJavaMast3r <scvolocaru@gmail.com>
2020-09-18 12:47:57 +0200

    Avance en la pantalla de carga

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Loading : MonoBehaviour
+{
+
+    VideoPlayer videoPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+    
+    void Update()
+    {
+        if (videoPlayer.frame == (long) videoPlayer.frameCount)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
