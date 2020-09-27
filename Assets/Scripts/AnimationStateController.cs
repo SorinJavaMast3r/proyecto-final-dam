69039503ad3a9f0b9d642721fe9a531224ffbfed
SorinJavaMast3r <scvolocaru@gmail.com>
2020-09-27 12:26:33 +0200

    Configuraciones del primer personaje y creacion de su prefab

---
+++ Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+
+    Animator animator;
+    int estaMoviendoHash;
+    int estaCorriendoHash;
+    int saltarHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        estaMoviendoHash = Animator.StringToHash("isWalking");
+        estaCorriendoHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool pulsarBoton = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d");
+        bool estaMoviendo = animator.GetBool(estaMoviendoHash);
+        bool correrPulsado = Input.GetKey("left shift");
+        bool estaCorriendo = animator.GetBool(estaCorriendoHash);
+        bool pulsarSaltar = Input.GetKey("space");
+
+        if (!estaMoviendo && pulsarBoton)
+        {
+            animator.SetBool(estaMoviendoHash, true);
+        }
+
+        if (estaMoviendo && !pulsarBoton)
+        {
+            animator.SetBool(estaMoviendoHash, false);
+        }
+
+        if (!estaCorriendo && (estaMoviendo && correrPulsado))
+        {
+            animator.SetBool(estaCorriendoHash, true);
+        }
+
+        if(estaCorriendo && (!estaMoviendo || !correrPulsado))
+        {
+            animator.SetBool(estaCorriendoHash, false);
+        }
+    }
+}
